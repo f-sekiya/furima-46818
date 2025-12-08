790b4a6ee735f61cce16a9b88ac4f01eada4960e
f-sekiya <f.sekiya.tech@gmail.com>
2025-12-08 20:19:00 +0900

    商品出品の基盤を実装（ActiveHash/ActiveStorage/関連/バリデーション）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,39 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
+  has_one_attached :image
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+
+  validates :price,
+            format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' },
+            allow_blank: true
+
+  validates :price,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 300,
+              less_than_or_equal_to: 9_999_999
+            },
+            allow_blank: true
 end
