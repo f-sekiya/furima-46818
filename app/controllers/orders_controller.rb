8c17421c761c0e5410f9b6fe23028854ebd45d0b
f-sekiya <f.sekiya.tech@gmail.com>
2025-12-15 17:44:46 +0900

    トークン化JSを実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,19 +4,47 @@ class OrdersController < ApplicationController
   before_action :move_to_index
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      return redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render 'index', status: :unprocessable_entity
+    end
   end
 
   private
+
+  def order_params
+    params.require(:order_address).permit(
+      :postal_code, :prefecture_id, :city, :address_line, :building, :phone_number, :token
+    ).merge(
+      user_id: current_user.id,
+      item_id: @item.id
+    )
+  end
+
   def set_item
     @item = Item.find(params[:item_id])
   end
 
   def move_to_index
-    if @item.order.present? || @item.user_id == current_user.id
-      redirect_to root_path
-    end
+    redirect_to root_path if @item.order.present? || @item.user_id == current_user.id
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
