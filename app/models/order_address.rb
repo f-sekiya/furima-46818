2ff724258a9262bb64d26143b06f689990480f1e
f-sekiya <f.sekiya.tech@gmail.com>
2025-12-15 15:51:54 +0900

    購入用Formオブジェクトと関連モデルを実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,31 @@
+class OrderAddress
+  include ActiveModel::Model
+
+  attr_accessor :user_id, :item_id, :token,
+                :postal_code, :prefecture_id, :city, :address_line, :building, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :token
+
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
+    validates :city
+    validates :address_line
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      address_line: address_line,
+      building: building,
+      phone_number: phone_number,
+      order_id: order.id
+    )
+  end
+end
